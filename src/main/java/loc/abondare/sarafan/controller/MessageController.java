be26f5eaf8150620841317b564c728fbed805583
BonDyka <bondarew@gmail.ru>
2021-07-17 15:44:02 +0300

    Lesson 8 Connect WebSocket(SockJS and Stomp)

--- src/main/java/loc/abondare/sarafan/controller/MessageController.java
+++ src/main/java/loc/abondare/sarafan/controller/MessageController.java
@@ -5,6 +5,8 @@ import loc.abondare.sarafan.domain.Message;
 import loc.abondare.sarafan.domain.Views;
 import loc.abondare.sarafan.repo.MessageRepo;
 import org.springframework.beans.BeanUtils;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -52,5 +54,11 @@ public class MessageController {
     @DeleteMapping("{id}")
     public void delete(@PathVariable("id") Message message) {
         messageRepo.delete(message);
+    }
+
+    @MessageMapping("/changeMessage")
+    @SendTo("/topic/activity")
+    public Message change(Message message) {
+        return messageRepo.save(message);
     }
 }
